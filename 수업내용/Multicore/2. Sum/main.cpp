ed96d19273933d2744153750690ef21985564874
Seungjun Lee <beeutiful@naver.com>
2023-09-08 15:17:47 +0900

    230908 수업내용
    . mutex, atomic
    - false sharing

--- 수업내용/Multicore/2. Sum/main.cpp
+++ 수업내용/Multicore/2. Sum/main.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
+#include <vector>
 using namespace std;
 
-int sum;
-mutex m;
+class SpinLock {
+public:
+	void lock()
+	{
+		bool expected = false;
+		while (!locked.compare_exchange_strong(expected, true)) {}
+	}
+	void unlock()
+	{
+		locked.store(false);
+	}
+private:
+	atomic_bool locked;
+};
 
-void func()
+volatile atomic_int sum;
+SpinLock m;
+
+void func(int n)
 {
-	auto s = 0;
-	for (auto i = 0; i < 25'000'000; ++i) {
+	volatile int s = 0;
+	for (auto i = 0; i < (50'000'000 / n); ++i) {
 		s += 2;
 	}
-	m.lock();
-	sum += s;
-	m.unlock();
+	sum.fetch_add(s, std::memory_order_relaxed);
 }
 
 int main()
 {
-	thread t1(func);
-	thread t2(func);
-	t1.join(); t2.join();
+	for (int i = 1; i < 64; i *= 2) {
+		sum = 0;
+		vector<thread> v;
+		auto t = chrono::high_resolution_clock::now();
+		for (int j = 0; j < i; ++j) {
+			v.emplace_back(func, i);
+		}
+		for (auto& t : v) t.join();
+		auto d = chrono::high_resolution_clock::now() - t;
 
-	cout << sum << endl;
+		cout << i << " threads : " << chrono::duration_cast<chrono::milliseconds>(d).count() 
+			<< " millisec, ans : " << sum << endl;
+	}
+
 }
